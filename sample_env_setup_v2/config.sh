252408fe4820eddd95cb5d3b4be68f564c0dba51
AhmedButt <ahmed.butt91@gmail.com>
2020-09-28 12:07:15 +0100

    Added final form of Script dir with v2

---
+++ sample_env_setup_v2/config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BD_HOST='db3.i'
+DB_NAME='ahmed_testing_tons'
+USER_NAME='ahmed_testing_tons'
+DB_PASSWORD=''
+DB_NAME_ENV="'ahmed_testing_tons',"
+USER_NAME_ENV="'ahmed_testing_tons',"
+DB_PASSWORD_ENV=""
+# DB_NAME_ENV="'ahmed_tons',"
+# USER_NAME_ENV="'ahmed_tons',"
+# DB_PASSWORD_ENV="'LzuGP9wzBhOsPDry',"
+# DB_NAME='ahmed_tons'
+# USER_NAME='ahmed_tons'
+# DB_PASSWORD='LzuGP9wzBhOsPDry'
+LIVE_PATH='/microcloud/domains/tondev/domains/dev10.tonsoftiles.co.uk/http/'
+DEV_PATH='/microcloud/domains/tondev/domains/ahmed.tonsoftiles.co.uk/http/'
+DB_TABLE='core_config_data'
+URL='http://ahmed.tonsoftiles.co.uk/'
+# T_DB_NAME='Tester_ton_test'
---
+++ sample_env_setup_v2/main_script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+source config.sh
+# source db_user.sh
+# This should be at the top of the script to get the start time of the script
+# start=$(date +%s.%N)
+
+echo ".................................."
+echo "..........DB CREATION............."
+echo ".................................."
+
+# time ./parse_db_data.sh -c
+
+echo "................................."
+echo "..........DATA RSYNC............."
+echo "................................."
+# Here you can place your function
+# rsync -zzarvh --exclude={'/pub/media','/pub/static','/var/page_cache','/var/cache','/var/full_page_cache','/var/session','/var/log','/generated'} ${LIVE_PATH} ${DEV_PATH}
+# end="$(date +%s.%N)/10000000"
+# duration=$(echo "$end - $start")
+# execution_time="printf "%.f seconds" $duration"
+rsync -zzarvh --exclude '/pub/media' ${LIVE_PATH} ${DEV_PATH}
+# echo "Script Execution Time: $execution_time"
+
+# cp -rvp ${LIVE_PATH} ${DEV_PATH}
+
+echo "................................."
+echo "..........PRODUCTION............."
+echo "................................."
+
+# time ./parse_db_data.sh -p
+
+echo "................................."
+echo "..........DEVELOPMENT............."
+echo "................................."
+
+# time ./parse_db_data.sh -d
+
+echo "..................................."
+echo "..........DATABASE_URL............."
+echo "..................................."
+
+# time ./parse_db_data.sh -u
+
+time ./parse_db_data.sh -cpdu
+echo "..................................."
+echo "..........CLEANINGCACHE............"
+echo "..................................."
+cd ${DEV_PATH}
+php-7.1 bin/magento c:f
