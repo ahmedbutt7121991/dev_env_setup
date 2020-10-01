2dcf9f826af7827e4ffdf931081356c73907f537
AhmedButt <ahmed.butt91@gmail.com>
2020-10-01 15:33:35 +0100

    looping all domains and vhosts

--- secure_site.sh
+++ secure_site.sh
@@ -2,38 +2,58 @@
 
 echo "Securing site"
 
+initial=/microcloud/domains/
+
+cd
+cd ${initial}
+domains=( $( ls . ))
+echo $domains
+
+for domain in *
+do
+    echo $domain
+    if [ $domain != 'lost+found' ]
+    then
+        echo $domain
+        cd ${initial}${domain}/domains/
+        for vhost in *
+        do
+            echo "${domain} --->>> ${vhost}"
+        done
+    fi
+done
 # names=(mubashir raisa sajid staging tauseef testing umar uzair usama waseem)
 # names=(bath bathtest bathtest1 bathtest2 cdn dev2 m2)
-names=test
-for i in "${names[@]}"; do
-    # printf ${i}
-    domain=cretes
-    vhost=${i}cretesol
-    echo ${vhost}
-    confile=/microcloud/domains/cretes/domains/${i}.cretesol.com/___general/${i}.cretesol.com.conf
-    # echo ${confile}
-    url=/microcloud/domains/cretes/domains/${i}.cretesol.com/___general
-    # echo ${url}
+# names=test
+# for i in "${names[@]}"; do
+#     # printf ${i}
+#     domain=cretes
+#     vhost=${i}cretesol
+#     echo ${vhost}
+#     confile=/microcloud/domains/cretes/domains/${i}.cretesol.com/___general/${i}.cretesol.com.conf
+#     # echo ${confile}
+#     url=/microcloud/domains/cretes/domains/${i}.cretesol.com/___general
+#     # echo ${url}
 
-    cd ${url}
-    pwd
-    touch .htpasswd
-    printf "${vhost}:$(openssl passwd -1 password)\n" >> .htpasswd
+#     cd ${url}
+#     pwd
+#     touch .htpasswd
+#     printf "${vhost}:$(openssl passwd -1 password)\n" >> .htpasswd
 
-    printf "location ~* ^/(index.php/)?dbscripts {
-    satisfy any;
+#     printf "location ~* ^/(index.php/)?dbscripts {
+#     satisfy any;
 
-    #allow x.x.x.x
+#     #allow x.x.x.x
 
-    auth_basic "Login";
-    auth_basic_user_file ${url}/.htpasswd;
+#     auth_basic "Login";
+#     auth_basic_user_file ${url}/.htpasswd;
 
-    deny all;
-    location ~* \.(php)$ {
-    include fastcgi_params;
-    }
-    try_files $uri $uri/ @bootstrap;
-    }" >> ${confile}
+#     deny all;
+#     location ~* \.(php)$ {
+#     include fastcgi_params;
+#     }
+#     try_files $uri $uri/ @bootstrap;
+#     }" >> ${confile}
 
-    /etc/init.d/nginx reload
-done
+#     /etc/init.d/nginx reload
+# done
